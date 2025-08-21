3ea18c15fb437b57e543c4bf1cc4b14f44e74d93
Mihai Zecheru <zecheruchris@gmail.com>
2025-08-21 13:40:11 -0700

    Screen cycles every 7.5s

--- smart_clock.ino
+++ smart_clock.ino
@@ -6,13 +6,17 @@ TFT_eSPI tft = TFT_eSPI();
 #include "Enums.h"
 #include "Screens.h"
 
+#define switch_delay 7500
+
 void setup()
 {
   init_tft();
-  draw_home_screen(15, 5, Tuesday, April, 19);
 }
 
 void loop()
 {
-
+  draw_home_screen(15, 5, Tuesday, April, 19);
+  delay(switch_delay);
+  draw_weather_screen(Rainy, 19, 25, 40, 60, 5, 10, 7, 8, 60, 100, 1.15, 3.25);
+  delay(switch_delay);
 }
